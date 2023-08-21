bcbaf8152ed65e945c70bf5c6303301b31135083
BritDua <68259529+BritDua@users.noreply.github.com>
2023-08-21 11:48:44 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket100"
+    bucket = "arn:aws:s3:::my-aws-tf-state-bucket100"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table100"
+    dynamodb_table = "arn:aws:dynamodb:us-east-2:892488123104:table/my-dynamo-db-table100"
   }
 }
